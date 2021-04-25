627f310ce04ea05ec5d99ee46e9b6e2a0d29f614
David Snipp <dave@stardraw.com>
2021-04-25 19:38:15 +0100

    try again

--- docs/ghspa.js
+++ docs/ghspa.js
@@ -23,6 +23,7 @@
 
   /* redirect all 404 trafic to index.html */
   function redirect() {
+    console.log("redirect", l)
     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
       (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
       (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
@@ -31,6 +32,7 @@
 
   /* resolve 404 redirects into internal routes */
   function resolve() {
+    console.log("resolve", l)
     if (l.search) {
       var q = {}
       l.search.slice(1).split('&').forEach(function (v) {
