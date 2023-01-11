edb92df780b6f4088ccba297343390dbf7b46f17
David Snipp <dave@stardraw.com>
2023-01-11 23:53:10 +0000

    Fix jsonp

--- src/app/contact.component.ts
+++ src/app/contact.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { catchError, map } from 'rxjs/operators'
-import { of } from 'rxjs'
+import { map, catchError, of, shareReplay } from 'rxjs'
 
 @Component({
   templateUrl: './contact.component.html'
 })
 export class ContactComponent {
   options = { center: { lat: 51.277002, lng: -0.610312 }, zoom: 15 }
-  apiLoaded = this.httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAga8m5cIZJL-WmWlH2w1fjh5iRhwTgx04', 'callback')
+  apiLoaded?: any
+  constructor(private httpClient: HttpClient) {
+    this.apiLoaded = this.httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAga8m5cIZJL-WmWlH2w1fjh5iRhwTgx04', 'callback')
     .pipe(map(_ => true), catchError(err => {
       console.log(err)
       return of(false)
-    }))
-  constructor(private httpClient: HttpClient) { }
+    }), shareReplay(1))
+  }
 }
